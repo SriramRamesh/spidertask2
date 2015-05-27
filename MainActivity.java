f540466f3f5518edd00a0be1f7b6b3491bade76b
SriramRamesh <grksriram321@gmail.com>
2015-05-27 22:22:41 +0530

    SpiderTask2full

--- MainActivity.java
+++ MainActivity.java
@@ -1,33 +1,40 @@
 package com.example.android.spidersubmsn2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
-  public static String[] A =new String[2];
-
-    @Override
+  public static String[] A =new String[3];
+    public static int Flag[]=new int[3];
+  @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText t=(EditText)findViewById(R.id.editText);
+        t.setText("Enter Text");
         String[] Size=new String[35];
         Size[0]="Size";
         for(int i=1;i<=30;i++)
             Size[i]=Integer.toString(i);
-        Spinner s1=(Spinner)findViewById(R.id.Spinner);
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item,Size);
+      Spinner s1=(Spinner)findViewById(R.id.Spinner);
+      Spinner s3=(Spinner)findViewById(R.id.Spinner3);
+      String Fonts[]={"Lobster","Great Vibes","Open Sans","Chunk Five","Alex Brush","Good Dog","Quick Sand"};
+      ArrayAdapter<String> adapter=new ArrayAdapter<String>(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item,Size);
         adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
         s1.setAdapter(adapter);
-     }
+      ArrayAdapter<String> adapter2=new ArrayAdapter<String>(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item,Fonts);
+      adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+      s3.setAdapter(adapter2);
+  }
 
 
     @Override
@@ -36,72 +43,46 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-    public static int Flag[]=new int[3];
-
-    public void RadioOnClick(View view)
+    public void RadioOnClick1(View view)
       {
-          boolean checked=((RadioButton)view).isChecked();
-          switch(view.getId())
-          {
-              case R.id.radioButton:
-                   if(checked)
-                       Flag[0]=1;
-                  else
-                       Flag[0]=0;
-                   break;
-              case R.id.radioButton2:
-                  if(checked)
-                      Flag[1]=1;
-                  else
-                      Flag[1]=0;
-                  break;
-              case R.id.radioButton3:
-                  if(checked)
-                      Flag[2]=1;
-                  else
-                      Flag[2]=0;
-                  break;
-          }
+          Flag[0]=1;
 
 
       }
+    public void RadioOnClick2(View view)
+    {
+        Flag[1]=1;
+
+
+    }
+    public String getTEXT()
+    {
+        EditText e=(EditText)findViewById(R.id.editText);
+        String S=e.getText().toString();
+        if(S.equals("Enter Text")) {
+          Toast t=Toast.makeText(getApplicationContext(), "Please enter text", Toast.LENGTH_LONG);
+            t.show();
+        }
+            return S;
+    }
 
     public void Message(View v)
     {
         //Saving EditText to a String S
-        EditText e=(EditText)findViewById(R.id.editText);
-        String S=e.getText().toString();
+        String S=getTEXT();
+        if(S.equals("Enter Text"))
+            return ;
+
         //Declaring Spinners for size and color
         Spinner s1=(Spinner)findViewById(R.id.Spinner);
         Spinner s2=(Spinner)findViewById(R.id.Spinner2);
-        s1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                A[0]=parent.getSelectedItem().toString();
-            }
-
-
-            public void onNothingSelected(AdapterView<?> arg0) {
-                A[0]="size";
-
-            }
-        });
-        s2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                A[1]=parent.getSelectedItem().toString();
-            }
-
-
-            public void onNothingSelected(AdapterView<?> arg0) {
-                A[1]="color";
-
-            }
-        });
-       /* Intent intent=new Intent(MainActivity.this,MainActivity2Activity.class);
+        Spinner s3=(Spinner)findViewById(R.id.Spinner3);
+        A[0]=s1.getSelectedItem().toString();
+        A[1]=s2.getSelectedItem().toString();
+        A[2]=s3.getSelectedItem().toString();
+        Intent intent=new Intent(MainActivity.this,MainActivity2Activity.class);
         intent.putExtra("EditText",S).putExtra("RadioFlags",Flag).putExtra("Size and color",A);
         startActivity(intent);
-        */
     }
 
     @Override
--- MainActivity2Activity.java
+++ MainActivity2Activity.java
@@ -1,9 +1,14 @@
 package com.example.android.spidersubmsn2;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity2Activity extends ActionBarActivity {
@@ -12,14 +17,89 @@ public class MainActivity2Activity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity2);
-        /*Intent intent=getIntent();
+        Intent intent=getIntent();
         String[] A=intent.getStringArrayExtra("Size and color");
         String Message =intent.getStringExtra("EditText");
         int Flag[]=intent.getIntArrayExtra("RadioFlags");
         TextView t=(TextView)findViewById(R.id.textView);
-        t.setText(Flag[0]);*/
+        t.setText(Message);
+        if(Flag[0]==1&&Flag[1]==1)
+        {
+            t.setTypeface(null, Typeface.BOLD_ITALIC);
+        }
+        else
+        {
+            if (Flag[1]==1)
+            {
+                t.setTypeface(null, Typeface.ITALIC);
+            }
+            if(Flag[0]==1)
+            {
+                t.setTypeface(null, Typeface.BOLD);
+            }
+        }
+
+        if(!A[0].equals("Size")) {
+            int size = Integer.parseInt(A[0]);
+            t.setTextSize(13 + size);
+        }
+        String color=A[1];
+       if(color.equals("bright_pink"))
+              t.setTextColor(Color.parseColor("#ee09e4"));
+       else if(color.equals("red"))
+                t.setTextColor(Color.RED);
+       else if(color.equals("orange"))
+            t.setTextColor(Color.parseColor("#ee8809"));
+       else if(color.equals("yellow"))
+            t.setTextColor(Color.YELLOW);
+       else if(color.equals("green"))
+            t.setTextColor(Color.GREEN);
+       else if(color.equals("blue"))
+            t.setTextColor(Color.BLUE);
+       else if(color.equals("violet"))
+            t.setTextColor(Color.parseColor("#7F00FF"));
+       else if(color.equals("magenta"))
+            t.setTextColor(Color.MAGENTA);
+        else
+            t.setTextColor(Color.WHITE);
+
+        String Font=A[2];
+        String loc;
+        if(Font.equals("Lobster"))
+            loc="Fonts/Lobster.otf";
+        else if(Font.equals("Great Vibes"))
+            loc="Fonts/GreatVibes-Regular.otf";
+        else if(Font.equals("Open Sans"))
+            loc="Fonts/OpenSans-Light.ttf";
+        else if(Font.equals("Chunk Five"))
+            loc="Fonts/Chunkfive.otf";
+        else if(Font.equals("Alex Brush"))
+            loc="Fonts/AlexBrush-Regular.ttf";
+        else if(Font.equals("Good Dog"))
+            loc="Fonts/GoodDog.otf";
+        else if(Font.equals("Quick Sand"))
+            loc="Fonts/Quicksand-Light.otf";
+        else
+            loc="None";
+        if(!loc.equals("None")) {
+            Typeface type = Typeface.createFromAsset(getAssets(),loc);
+            t.setTypeface(type);
+        }
+}
+    public void GotoMainActivity(View v)
+    {
+        Intent in=new Intent(MainActivity2Activity.this,MainActivity.class);
+        startActivity(in);
+    }
+    public void ExitApp(View v)
+    {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
 
     }
+
 
 
     @Override
